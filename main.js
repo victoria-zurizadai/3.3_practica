adbf62631288564fcd7d62f6e5436cbd21f7cd36
victoria-zurizadai <l17200792@pachuca.tecnm.mx>
2021-06-03 09:18:44 -0500

    Actualización 3D

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.getElementById("btn1").addEventListener("click",(e)=>{
    var el=document.querySelector('#id01');
-   el.setAttribute("obj-model", 'obj:url(bebidas.obj);mtl:url(bebidas.mtl)');
+   el.setAttribute("obj-model", 'obj:url(comedor.obj);mtl:url(comedor.mtl)');
    el.setAttribute("scale", '.10 .10 .10');
  })
  document.getElementById("btn2").addEventListener("click",(e)=>{
@@ -18,6 +18,6 @@ document.getElementById("btn3").addEventListener("click",(e)=>{
 document.getElementById("btn4").addEventListener("click",(e)=>{
 
    var el=document.querySelector('#id01');
-   el.setAttribute("obj-model", 'obj:url(REDE.obj);mtl:url(REDE.mtl)');
+   el.setAttribute("obj-model", 'obj:url(bebidas.obj);mtl:url(bebidas.mtl)');
    el.setAttribute("scale", '.10 .10 .10');
 })
